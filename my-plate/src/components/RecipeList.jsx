f2ffb8477ec908596a730806d57082c2efae0e14
Alvin Toch <Alvinstoch@live.com>
2022-12-05 22:07:07 -0800

    I don't think there are any significant changes, I'm just trying to update my file

--- my-plate/src/components/RecipeList.jsx
+++ my-plate/src/components/RecipeList.jsx
@@ -4,7 +4,7 @@ import {useNavigate} from "react-router-dom"
 
 export default function RecipeList () {
     const [recipe, setRecipe]= useState([])
-    const [user,setUser]=useState([])
+    // const [user,setUser]=useState([])
     //let userId = recipe.user_id
     const navigate = useNavigate();
     useEffect(()=>{
@@ -13,8 +13,8 @@ export default function RecipeList () {
             const response = await axios.get(`http://localhost:3001/api/recipes/`);
             setRecipe(response.data)
 
-            const response2= await axios.get(`http://localhost:3001/api/users/`)
-            setUser(response2.data)
+            // const response2= await axios.get(`http://localhost:3001/api/users/`)
+            // setUser(response2.data)
             // const response3= await axios.get(`http://localhost:3001/api/reviews/`)
         } catch(e){
             console.log(`please hold`)
@@ -29,15 +29,24 @@ const goToRecipe = (x)=>{
     navigate(`/displayrecipe/${x.id}`)
     // navigate(`/randomrecipe`)
 }
+const goToUser = (x)=>{
+    navigate(`/user/${x.user.id}`)
+}
 
-// console.log(user[1])
+console.log(recipe)
 // console.log(user)
     return (
+        (!recipe)?
+        <h2>Loading...</h2>
+        :
         <div className="test-wrapper">
         <h3>RecipeList</h3>
         <div >{
             recipe.map((x)=>(
-                <div onClick={()=>goToRecipe(x)} key={x.id}>{x.name} </div>
+                <div>
+                <div onClick={()=>goToRecipe(x)} key={x.id}>{x.name} by {x.user.username} </div>
+                <div></div>
+                </div>
             ))
             }</div>
         </div>

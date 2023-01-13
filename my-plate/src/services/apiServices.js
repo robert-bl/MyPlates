603fd57202e935c92458d79674f05983f2987bf6
Robert Buskirk-Lechner <buskirklechner@gmail.com>
2023-01-13 14:22:02 -0500

    fixed app_secret in middleware

--- my-plate/src/services/apiServices.js
+++ my-plate/src/services/apiServices.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = 'http://localhost:3001'
+export const BASE_URL = 'https://git.heroku.com/myplates-hapi.git'
 
 const axiosCreate = axios.create({ baseURL: BASE_URL})
 
